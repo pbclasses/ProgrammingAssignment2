aff11b77611d4cb16cc6450c3d356c93cf05c217
Phil Bangayan <bangayan@phils-mbp.attlocal.net>
2017-10-20 06:23:12 -0700

    wrote functions makeCacheMatrix() and cacheSolve() based on sample functions makeVector() and cachemean()

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions makeCahceMatrix() and cacheSolve() allow for
+## calculating and efficiently storing the inverse of a matrix.
+## If a matrix inverse has not been determined, it is
+## calculated and cached. If the matrix inverse has already
+## been cached, that value is retrieved rather than 
+## re-calculated.
 
-## Write a short comment describing this function
+## makeCacheMatrix returns a list of four functions.
+## usage: mm = makeCacheMatrix(mdat)
+## returns: mm$get(), mm$set(m), mm$getinv(), mm$setinv(inv)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the inverse of a matrix, either
+## calculating it for the first time or a cached version
+## usage: minv <- cacheSolve(mm)
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
